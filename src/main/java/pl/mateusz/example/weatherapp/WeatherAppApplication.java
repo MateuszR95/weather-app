6fd5c9d05b975f129c641e842d95975deac7ab45
Mateusz Rojek <mateuszr2047@gmail.com>
2024-05-11 13:30:38 +0200

    Utworzenie zadania

---
+++ src/main/java/pl/mateusz/example/weatherapp/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.example.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppApplication.class, args);
+    }
+
+}
